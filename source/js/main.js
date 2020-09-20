c1cdbf561246f6803ba88d6a1fa0b08b7b71fdcc
Martin Maly <maly@maly.cz>
2020-09-20 15:29:59 +0200

    dist

---
+++ source/js/main.js
@@ -0,0 +1,9 @@
+// npx @adent/parcel-boiler-es6
+window.$ = window.jQuery = require("jquery"); //require("./node_modules/jquery/dist/jquery.min.js");
+require("popper.js");
+require("bootstrap/js/src");
+require("jquery-ui-dist/jquery-ui.js");
+require("malihu-custom-scrollbar-plugin")
+require("./prism.min.js")
+
+require("./theme-main.js")
